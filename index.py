978c0dcc3a59e0b464dfba8f79eca346d8e2fe4e
Rounak Agarwal <rounakr.ag73@gmail.com>
2019-08-23 01:07:35 +0530

    Add buffer to account for time spent in api requests
    Signed-off-by: Rounak Agarwal <rounakr.ag73@gmail.com>

--- index.py
+++ index.py
@@ -14,10 +14,20 @@ while True:
 
 print("Fetching questions...")
 
+prev_id = []
+current_id = []
+
 while True:
-    fromDate = time.time()
+    if not prev_id:
+        fromDate = time.time()
+    else:
+        fromDate = time.time() - 10
     time.sleep(150)
     toDate = time.time()
+
+    print(fromDate, toDate, sep=", ")
+
+    # print(time.time())
 
     url = 'https://api.stackexchange.com/2.2/search/advanced?fromdate=' + str(round(fromDate)) + '&todate=' + str(
         round(toDate)) + '&order=asc&sort=creation&accepted=False&closed=False&tagged=' + tags + '&site=stackoverflow'
@@ -26,6 +36,18 @@ while True:
 
     print(r.json())
 
+    current_id.clear()
+
     for item in r.json()['items']:
-        print(item['link'])
-        print(item['title'])
+        current_id.append(item['question_id'])
+
+    next_id = [qid for qid in current_id if qid not in prev_id]
+
+    for item in r.json()['items']:
+        if item['question_id'] in next_id:
+            print(item['link'])
+            print(item['title'])
+
+    prev_id = next_id
+
+    # print(time.time())
